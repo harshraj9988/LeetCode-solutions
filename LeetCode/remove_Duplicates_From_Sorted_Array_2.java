dea21017e9959bda548231e34c13dd72ad31f16d
Harsh <harshraj9988@gmail.com>
2022-06-13 07:51:45 +0530

    uploading

--- LeetCode/remove_Duplicates_From_Sorted_Array_2.java
+++ LeetCode/remove_Duplicates_From_Sorted_Array_2.java
@@ -1,5 +1,5 @@
 /**
- *Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+ * Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
 
 Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
 
@@ -18,11 +18,11 @@ int k = removeDuplicates(nums); // Calls your implementation
 
 assert k == expectedNums.length;
 for (int i = 0; i < k; i++) {
-assert nums[i] == expectedNums[i];
+    assert nums[i] == expectedNums[i];
 }
 If all assertions pass, then your solution will be accepted.
 
-
+ 
 
 Example 1:
 
@@ -36,7 +36,7 @@ Input: nums = [0,0,1,1,1,1,2,3,3]
 Output: 7, nums = [0,0,1,1,2,3,3,_,_]
 Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
-
+ 
 
 Constraints:
 
@@ -45,16 +45,15 @@ Constraints:
 nums is sorted in non-decreasing order.
  */
 
-class remove_Duplicates_From_Sorted_Array_II{
-    fun removeDuplicates(nums: IntArray): Int {
-        var index = 0
-
-        for (num in nums){
-            if( index<2 || num > nums[index-2]){
-                nums[index++]=num
+public class remove_Duplicates_From_Sorted_Array_2 {
+    public int removeDuplicates(int[] nums) {
+        
+        int index =0;
+        for (int num : nums){
+            if( index< 2 || num > nums[index-2]){
+                nums[index++] = num;
             }
         }
-
-        return index
-    }
+        return index;
+    } 
 }
