6c6e444865b3c716c052b7f382440e1293c8cabb
harshraj9988 <harshraj9988@gmail.com>
2022-08-02 12:08:34 +0530

    2-aug-2022

--- LeetCode/cc.java
+++ LeetCode/cc.java
@@ -1,71 +1,24 @@
 import java.util.*;
 
 public class cc {
-    public static long findMaximumSum(List<Integer> stockPrices, int k) {
-        int n = stockPrices.size();
-
-        Set<Integer> set = new HashSet<>();
-        long maxSum = -1;
-
-        long sum = 0;
-
-        int t = k;
-
-        for (int i = 0; i < n ; i++) {
-            Integer sp = stockPrices.get(i);
-
-            if (t == 0) {
-                Integer temp = stockPrices.get(i - k);
-                sum -= temp;
-                set.remove(temp);
-
-                if (set.contains(sp)) {
-                    sum = 0;
-                    set.clear();
-                    i--;
-                    t = k;
-                    continue;
-                }
-
-                sum += sp;
-                set.add(sp);
-                
-            } else {
-                
-                t--;
-
-                if (set.contains(sp)) {
-                    sum = 0;
-                    set.clear();
-                    i--;
-                    t = k;
-                    continue;
-                }
-
-                sum += sp;
-                set.add(sp);
-               
-            }
-            maxSum = Math.max(sum, maxSum);
-            
+    
+    public int maximumGroups(int n) {
+       
+        int x = 1, y = 0;
+        while(n>0){
+            n -= x;
+            y++;
+            x++;
+            if(x>n) break;
         }
-
-        return maxSum;
+        return y;
     }
+
+    
 
     public static void main(String[] args) {
-        List<Integer> stockPrices = new ArrayList<>();
-        
-
-        String s = "1 2 7 7 4 3 6 3";
-
-        String[] arr = s.split(" ");
-        for (String i : arr) {
-            stockPrices.add(Integer.parseInt(i));
-        }
-        int k = stockPrices.remove(stockPrices.size()-1);
-
-        long ans = cc.findMaximumSum(stockPrices, k);
-        System.out.println(ans);
+       int n = 10;
+       System.out.println(new cc().maximumGroups(n));
     }
 }
+  
