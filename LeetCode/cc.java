316d46a4686fd44d7f77b866a970d22b6916f370
harshraj9988 <harshraj9988@gmail.com>
2022-08-11 07:02:18 +0530

    11-Aug-2022

--- LeetCode/cc.java
+++ LeetCode/cc.java
@@ -1,26 +1,5 @@
 import java.util.*;
 
 public class cc {
-    
-    public int maximumGroups(int n) {
-       
-        int x = 1, y = 0;
-        while(n>0){
-            n -= x;
-            y++;
-            x++;
-            if(x>n) break;
-        }
-        return y;
-    }
 
-    
-
-    public static void main(String[] args) {
-      int x = Integer.MAX_VALUE;
-      int y = Integer.MIN_VALUE;
-      System.out.println("max:"+(x+1));
-      System.out.println("min:"+y);
-    }
 }
-  
