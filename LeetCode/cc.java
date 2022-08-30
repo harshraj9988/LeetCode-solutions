d1a778d39deb829073c59926e4bdeadd072dc0c4
harshraj9988 <harshraj9988@gmail.com>
2022-08-30 09:00:26 +0530

    30-Aug-2022

--- LeetCode/cc.java
+++ LeetCode/cc.java
@@ -1,37 +1,42 @@
 import java.util.*;
+import java.util.Map.Entry;
+
+import javax.swing.GrayFilter;
 
 public class cc {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] nums = new int[n];
-        for(int i=0; i<n; i++){
-            nums[i] = in.nextInt();
+    public int[][] buildMatrix(int k, int[][] rowConditions, int[][] colConditions) {
+        Map<Integer, int[]> map = new HashMap<>();
+        for(int i=1; i<=k ; i++){
+            map.put(i, new int[]{0,0});
         }
-        System.out.println(binarySearchApproach(nums));
 
+        for(int[] i: rowConditions){
+            map.get(i[1])[0]= map.get(i[0])[0]+1;
+        }
+
+        for(int[] i: colConditions){
+            map.get(i[1])[1]= map.get(i[0])[1]+1;
+        }
+
+        int[][] ans = new int[k][k];
+
+
+        for(Entry<Integer, int[]> i:map.entrySet()){
+            int[] xy = i.getValue();
+            ans[xy[0]][xy[1]] = i.getKey();
+        }
+
+        for(int[] i: ans){
+            System.out.println(Arrays.toString(i));
+        }
+
+        return rowConditions;
     }
 
-    private static int binarySearchApproach(int[] nums) {
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        arr.add(nums[0]);
-        int size = 1;
-        int n = nums.length;
-        for (int i = 1; i < n; i++) {
-            int num = nums[i];
-            int ind = Collections.binarySearch(arr, num);
-            if (ind < 0) {
-                ind *= -1;
-                ind--;
-                if (ind == size) {
-                    arr.add(num);
-                    size++;
-                } else {
-                    arr.set(ind, num);
-                }
-            }
-        }
-        return size;
+    public static void main(String[] args) {
+        int k = 3;
+        int[][] rowConditions = {{1,2},{3,2}};
+        int[][] colConditions = {{2,1},{3,2}};
+        new cc().buildMatrix(k, rowConditions, colConditions);
     }
 }
--- LeetCode/shortest_Subarray_With_Sum_At_Least_K.java
+++ LeetCode/shortest_Subarray_With_Sum_At_Least_K.java
@@ -25,25 +25,28 @@ Constraints:
 -105 <= nums[i] <= 105
 1 <= k <= 109
  */
+import java.util.*;
 public class shortest_Subarray_With_Sum_At_Least_K{
     public int shortestSubarray(int[] nums, int k) {
-        int len = Integer.MAX_VALUE;
+        int shortest = Integer.MAX_VALUE;
 
+        Deque<long[]> deque = new ArrayDeque<>();
         int n = nums.length;
-        int i=0, j=0;
-        int sum = 0;
-        while(j<n){
-            if(sum+nums[j]<=nums[j]){
-                i=j;
-                sum = 0;
-            }
-            sum+=nums[j];
-            if(sum>=k){
-                len = Math.min(len, (j-i+1));
-            }
-            j++;
-        }
+        long sum = 0;
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
+            if(sum>=k) shortest = Math.min(shortest, i+1);
 
-        return len;
+            long[] pair = new long[]{Integer.MIN_VALUE, Integer.MIN_VALUE};
+            while(!deque.isEmpty() && (sum - deque.getFirst()[1])>=k){
+                pair = deque.removeFirst();
+            }
+            if(pair[0]!=Integer.MIN_VALUE){
+                shortest = Math.min(shortest, i-(int)pair[0]);
+            }
+            while(!deque.isEmpty() && sum<deque.getLast()[1]) deque.removeLast();
+            deque.add(new long[]{i, sum});
+        }    
+        return (shortest==Integer.MAX_VALUE)?-1: shortest;
     }
 }
