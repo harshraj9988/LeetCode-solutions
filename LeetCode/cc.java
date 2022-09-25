320d21cb555e59ff3d9cd9d525a35e4e1a587b2e
harshraj9988 <harshraj9988@gmail.com>
2022-09-25 07:53:06 +0530

    25-Sept-2022

--- LeetCode/cc.java
+++ LeetCode/cc.java
@@ -7,7 +7,26 @@ import java.util.*;
 
 class Solution {
 
-   
+    public String getEncryptedNumber(int[] numbers) {
+        int n = numbers.length;
+        for(int i=n-1; i>1; i--) {
+            for(int j=0; j<i; j++) {
+                numbers[j] = (numbers[j]+numbers[j+1])%10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(numbers[0]);
+        sb.append(numbers[1]);
+        return sb.toString();
+    }
 
     
+    
+}
+
+public class cc {
+    public static void main(String[] args) {
+        int[] numbers = {4,5};
+        System.out.println(new Solution().getEncryptedNumber(numbers));
+    }
 }
