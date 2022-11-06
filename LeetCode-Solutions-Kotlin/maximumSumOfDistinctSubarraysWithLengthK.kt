b44993ebb1f8c8c4c13542213504d4b21c1a1f77
harshraj9988 <harshraj9988@gmail.com>
2022-11-06 14:21:03 +0530

    6-Nov-2022

---
+++ LeetCode-Solutions-Kotlin/maximumSumOfDistinctSubarraysWithLengthK.kt
@@ -0,0 +1,12 @@
+import java.lang.Long.max
+import java.util.*
+
+fun maximumSubarraySum(nums: IntArray, k: Int): Long {
+
+}
+
+fun main() {
+    val nums = intArrayOf(1, 1, 1, 7, 8, 9)
+    val k = 3
+    println(maximumSubarraySum(nums, k))
+}
