1bf7e5106164c30ca51a165ecab9a960661e0d6d
Harsh Raj <harshraj9988@gmail.com>
2023-03-17 19:27:02 +0530

    17-mar-23

---
+++ Leetcode-solutions-python/implementTriePrefixTree.py
@@ -0,0 +1,30 @@
+class Trie:
+
+    def __init__(self):
+        self.children = dict()
+        self.isWord = False
+
+    def insert(self, word: str) -> None:
+        node = self
+        for c in word:
+            if c not in node.children:
+                node.children[c] = Trie()
+            node = node.children[c]
+        node.isWord = True
+        return
+
+    def search(self, word: str) -> bool:
+        node = self
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.isWord
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
